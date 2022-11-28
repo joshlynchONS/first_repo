3099fed5889d0ef996f6a461f55dcf70bfd09611
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2022-11-28 13:29:34 +0000

    initial keras model

---
+++ src/make_models/initial_model.py
@@ -0,0 +1,20 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+def build_model(input_shape):
+    """a simple MLP model to predict binary labels of a dataset
+    Args:
+        input_shape (tuple): the shape of the data used in this model
+
+    Returns:
+        keras model: compiled MLP keras model
+    """
+
+    model = Sequential()
+    model.add(Dense(12, input_shape=(input_shape,), activation="relu"))
+    model.add(Dense(8, activation="relu"))
+    model.add(Dense(1, activation="sigmoid"))
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    return model
