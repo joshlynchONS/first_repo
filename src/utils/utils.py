4deef0a687127d162bd2c9de7282d153bf510655
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2022-12-19 17:37:56 +0000

    add utils file

---
+++ src/utils/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def calc_accuracy(predicted_test, labels_test):
+    """_summary_
+
+    Args:
+        predicted_test (numpy array): predicted values
+        labels_test (umpy array): actual values
+
+    Returns:
+        float: the accuracy between two arrays displayed as a percentage
+    """
+
+    rounded_prediction = np.around(predicted_test, decimals=0).astype(int)
+
+    predicted_vs_labels_arr = np.equal(rounded_prediction, labels_test)
+
+    number_true = np.count_nonzero(predicted_vs_labels_arr)
+
+    percentage_accuracy = (number_true / len(predicted_vs_labels_arr)) * 100
+
+    return percentage_accuracy
